e18c81d0567c07890a424cd0dd7e8ebfaafa9dc5
mistyrinth <sayaka.mistyrinth@gmail.com>
2019-05-20 16:42:27 +0900

    add omniauth Github

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: "users/registrations",
+    omniauth_callbacks: "users/omniauth_callbacks"
+  }
+
   root to: 'books#index'
   resources :books
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   scope "(:locale)" do
     resources :users
-    end
+  end
 
   # letter_opener_web
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
-
 end
